6db60eb6011ccc103ef1988ee8ef05665c62767d
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2022-11-21 12:58:21 +0600

    Cycle sort

---
+++ Sorting/CycleSort.java
@@ -0,0 +1,24 @@
+package Sorting;
+
+import java.util.Arrays;
+
+public class CycleSort {
+    public static void main(String[] args) {
+        int [] arr={4,5,6,1,2,3};
+        System.out.println(Arrays.toString(cycleSort(arr)));
+    }
+    static int[] cycleSort(int[]arr){
+        int i=0;
+        while (i< arr.length){
+            if (arr[i]!=i+1) {
+                int temp = arr[i];
+                arr[i] = arr[temp - 1];
+                arr[temp - 1] = temp;
+            }
+            else {
+                i++;
+            }
+        }
+        return arr;
+    }
+}
