8ecd411cc1603d90ee560617af8fd3eac4ea9f53
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-01-10 14:56:24 +0600

    palindrome check

--- Recursion/PalindromeCheck.java
+++ Recursion/PalindromeCheck.java
@@ -2,15 +2,18 @@ package Recursion;
 
 public class PalindromeCheck {
     public static void main(String[] args) {
-        int n=1231;
+        int n=1221;
         System.out.println(checkPalindrome(n));
     }
     static boolean checkPalindrome(int n){
-        int noOfDigits=(int) Math.log10(n)+1;
-        return (n==reverse(n,noOfDigits));
+        return (n==reverse(n));
     }
-    static int reverse(int n,int noOfDigits){
+    static int reverse(int n){
+        int noOfDigits=(int)Math.log10(n)+1;
+        return reverseHelper(n,noOfDigits);
+    }
+    static int reverseHelper(int n,int noOfDigits){
         if (n%10==n) return n;
-        return (n%10)*(int)(Math.pow(10,noOfDigits-1)) +reverse(n/10,noOfDigits-1);
+        return (n%10)*(int)(Math.pow(10,noOfDigits-1)) +reverseHelper(n/10,noOfDigits-1);
     }
 }
