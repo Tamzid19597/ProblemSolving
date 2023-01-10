a2434c467296c9a2131a5d97b52fe242b5fd77fc
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-01-10 13:49:47 +0600

    factorial of a number

---
+++ Recursion/FactorialOfANumber.java
@@ -0,0 +1,14 @@
+package Recursion;
+
+public class FactorialOfANumber {
+    public static void main(String[] args) {
+        int n=5;
+        System.out.println(factorial(n));
+    }
+    static int factorial(int n){
+        if (n<=1){
+            return 1;
+        }
+        else return n*factorial(n-1);
+    }
+}
