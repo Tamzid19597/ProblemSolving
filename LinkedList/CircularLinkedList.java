87739851b5bfd542a7a586ad0e965bcbfdea86f1
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-01-31 13:33:08 +0600

    circuler linkedlist create,insert,deletec,display

--- LinkedList/CircularLinkedList.java
+++ LinkedList/CircularLinkedList.java
@@ -1,6 +1,6 @@
 package LinkedList;
 
-public class SinglyLinkedList {
+public class CircularLinkedList {
     private Node head;
     private Node tail;
     private int size;
@@ -8,23 +8,27 @@ public class SinglyLinkedList {
         private int value;
         private Node next;
 
-        public Node(int value,Node next) {
+        public Node(int value, Node next) {
             this.value = value;
             this.next=next;
         }
     }
     void insertAtFirst(int value){
-        Node node=new Node(value,this.head);
+        Node node=new Node(value,null);
+        node.next=this.head;
         this.head=node;
-        if (this.tail==null) this.tail=this.head;
+        if (this.tail==null) {
+            this.tail=this.head;
+            this.head.next=this.tail;
+        }
         size++;
     }
     void insertAtLast(int value){
         Node node=new Node(value,null);
-        if (head==null) this.head=node;
+        if (this.head==null) this.head=node;
         this.tail.next=node;
         this.tail=node;
-
+        this.tail.next=this.head;
     }
     void insertAtPosition(int value,int index){
         if (index==0) {
@@ -37,7 +41,6 @@ public class SinglyLinkedList {
         }
         if (temp==null) System.out.println("Index out of reach!");
         else {
-            System.out.println(temp.value);
             Node node=new Node(value,temp.next);
             temp.next=node;
             if (node.next==null) this.tail=node;
@@ -50,27 +53,30 @@ public class SinglyLinkedList {
             return;
         }
         Node temp=this.head;
-        for (int i=1;i<index && temp!=null;i++){
+        for (int i=1;i<index && temp!=tail;i++){
             temp=temp.next;
         }
-        if (temp==null) System.out.println("Index out of reach!");
+        if (temp==this.tail) System.out.println("Index out of reach!");
 
         else temp.next=temp.next.next;
     }
     void displayList(){
-        Node temp=this.head;
-        while (temp!=null){
+        Node temp=this.head.next;
+        System.out.print(this.head.value+"==>");
+        while (temp!=this.head){
             System.out.print(temp.value+"==>");
             temp=temp.next;
         }
+        //System.out.print(this.tail.value+"==>");
         System.out.println("END");
     }
     public static void main(String[] args) {
-        SinglyLinkedList singlyLinkedList=new SinglyLinkedList();
-        singlyLinkedList.insertAtFirst(30);
-        singlyLinkedList.insertAtLast(15);
-        //singlyLinkedList.insertAtPosition(20,1);
-        singlyLinkedList.deleteAtPosition(5);
-        singlyLinkedList.displayList();
+        CircularLinkedList circularLinkedList=new CircularLinkedList();
+        circularLinkedList.insertAtFirst(30);
+        circularLinkedList.insertAtLast(15);
+        circularLinkedList.insertAtPosition(20,1);
+        circularLinkedList.deleteAtPosition(2);
+        circularLinkedList.displayList();
+        System.out.println(circularLinkedList.tail.value);
     }
 }
