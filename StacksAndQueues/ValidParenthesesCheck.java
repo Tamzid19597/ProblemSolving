8a8907fa3a0531612f5a694fbd0a435e7c183693
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-02-05 01:32:45 +0600

    https://leetcode.com/problems/valid-parentheses/description/

---
+++ StacksAndQueues/ValidParenthesesCheck.java
@@ -0,0 +1,26 @@
+package StacksAndQueues;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+//https://leetcode.com/problems/valid-parentheses/description/
+public class ValidParenthesesCheck {
+    static boolean isValid(String s) {
+        HashMap<Character,Character> map=new HashMap<>();
+        map.put('(',')');
+        map.put('{','}');
+        map.put('[',']');
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c))stack.push(map.get(c));
+            else if(stack.isEmpty() || stack.peek()!=(c)) return false;
+            else stack.pop();
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("([{}]) is Valid: "+isValid("([{}])"));
+        System.out.println("([{]}) is Valid: "+isValid("([{]})"));
+    }
+}
