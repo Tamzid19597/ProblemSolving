c4dde99c8005972a689acaab1c6cfa18fcc7944f
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-02-05 00:54:23 +0600

    public int subarraySum(int[] nums, int k) {

---
+++ MapHashMapAndHashSet/SubarraySumEqualsK.java
@@ -0,0 +1,27 @@
+package MapHashMapAndHashSet;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    static int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> hashMap=new HashMap<>();
+        hashMap.put(0,1);
+        int sum=0;
+        int totalSubArr=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            if(hashMap.containsKey(sum-k))totalSubArr+=hashMap.get(sum-k);
+            if(hashMap.containsKey(sum))hashMap.put(sum,hashMap.get(sum)+1);
+            else hashMap.put(sum,1);
+        }
+        return totalSubArr;
+    }
+
+    public static void main(String[] args) {
+        int [] arr={0,0,0,0};
+        int k=0;
+        System.out.println(Arrays.toString(arr)+",K="+k);
+        System.out.println(subarraySum(arr,k));
+    }
+}
---
+++ StacksAndQueues/LargestRectangleInHistogram.java
@@ -0,0 +1,48 @@
+package StacksAndQueues;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+
+public class LargestRectangleInHistogram {
+    static int largestRectangleArea(int[] heights) {
+        HashSet<Integer> hashSet=new HashSet<>();
+        int[]leftBarArea=new int[heights.length];
+        int[]rightBarArea=new int[heights.length];
+        for(int i=0;i<heights.length;i++){
+            int area=heights[i];
+            for(int j=i-1;j>=0;j--){
+                if(heights[i]==heights[j]){
+                    area+=leftBarArea[j];
+                    break;
+                }
+                else if(heights[i]<heights[j])area+=heights[i];
+                else break;
+            }
+            leftBarArea[i]=area;
+        }
+        for(int i=heights.length-1;i>=0;i--){
+            int area=0;
+            for(int j=i+1;j<heights.length;j++){
+                if(heights[i]==heights[j]){
+                    area+=rightBarArea[j];
+                    break;
+                }
+                else if(heights[i]<heights[j])area+=heights[i];
+                else break;
+            }
+            rightBarArea[i]=area;
+        }
+        for(int i=0;i<heights.length;i++){
+            hashSet.add(rightBarArea[i]+leftBarArea[i]);
+        }
+        return  Collections.max(hashSet);
+    }
+
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5};
+        System.out.println(Arrays.toString(arr));
+        System.out.println(largestRectangleArea(arr));
+        System.out.println();
+    }
+}
