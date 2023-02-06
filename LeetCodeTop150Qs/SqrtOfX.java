ecff4055cc436d6a5a6ced9e2bfd12f27f03ed7b
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-02-07 02:40:56 +0600

    https://leetcode.com/problems/sqrtx/description/

---
+++ LeetCodeTop150Qs/SqrtOfX.java
@@ -0,0 +1,14 @@
+package LeetCodeTop150Qs;
+
+public class SqrtOfX {
+    static int mySqrt(int x) {
+        long r = x;
+        while (r*r > x)
+            r = (r + x/r) / 2;//newtons law
+        return (int) r;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(mySqrt(8));
+    }
+}
