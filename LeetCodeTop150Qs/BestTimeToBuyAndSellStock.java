972725a6cddcdb2e9e0f290f2bf08562ccc16417
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2023-02-08 23:25:28 +0600

    https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

---
+++ LeetCodeTop150Qs/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,21 @@
+package LeetCodeTop150Qs;
+//https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+public class BestTimeToBuyAndSellStock {
+    static int maxProfit(int[] prices) {
+        int maxProfit=0;
+        int buyingPrice=prices[0]+1;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<buyingPrice){
+                buyingPrice=prices[i];
+            }
+            int profit=prices[i]-buyingPrice;
+            if(profit>maxProfit)maxProfit=profit;
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,3,4,6,2}));
+    }
+
+}
